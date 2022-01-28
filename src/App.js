194e071f9bcacc0602dfe485250ae78874870f91
Kazuo Abduch <76076430+kazuo-abduch@users.noreply.github.com>
2022-01-28 18:54:54 -0300

    Add context

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import Routes from './Routes/Routes';
+import RecipeContext from './Context/RecipeContext';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <Routes />
+    <RecipeContext.Provider>
+      <Routes />
+    </RecipeContext.Provider>
   );
 }
 
