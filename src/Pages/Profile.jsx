b75572f8648d6380def2e23b88df9814a2581d24
Otavio Cantareli <otavio_cantareli@outlook.com>
2022-02-01 16:03:29 -0300

    Pronto

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../Components/Footer';
 import Header from '../Components/Header';
 import RecipeContext from '../Context/RecipeContext';
@@ -8,12 +9,48 @@ function Profile() {
   useEffect(() => {
     setSearchBtn(false);
   });
+  const email = JSON.parse(localStorage.getItem('user'));
+  const history = useHistory();
+  const favoriteRedirect = () => {
+    history.push('/favorite-recipes');
+  };
+  const doneRedirect = () => {
+    history.push('/done-recipes');
+  };
+  const logout = () => {
+    localStorage.clear();
+    history.push('/');
+  };
   return (
     <>
       <Header pageName="Profile" />
+      <div>
+        <span data-testid="profile-email">{email.email}</span>
+        <button
+          type="button"
+          data-testid="profile-done-btn"
+          onClick={ doneRedirect }
+        >
+          Done Recipes
+        </button>
+        <button
+          type="button"
+          data-testid="profile-favorite-btn"
+          onClick={ favoriteRedirect }
+        >
+          Favorite Recipes
+        </button>
+        <button
+          type="button"
+          data-testid="profile-logout-btn"
+          onClick={ logout }
+        >
+          Logout
+        </button>
+      </div>
       <Footer />
     </>
   );
 }
-// teste
+
 export default Profile;
